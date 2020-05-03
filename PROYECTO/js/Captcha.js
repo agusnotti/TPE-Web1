72f70cb764d300052974f4441ca48115844eb9fa
agusnotti <agustinantt@gmail.com>
2020-05-03 00:36:57 -0300

    ultimos detalles

--- PROYECTO/js/Captcha.js
+++ PROYECTO/js/Captcha.js
@@ -11,21 +11,20 @@ input.addEventListener('input',function(){
 
 let captcha = document.querySelector("#captcha-codigo");
 captcha.innerHTML = (Math.round(Math.random() * 9000 + 1000));
-let valor = document.querySelector("#input-captcha");
+let inputUsuario = document.querySelector("#input-captcha");
 
 function validar() {
-    let valor = document.querySelector("#input-captcha");
     let textocaptcha= document.querySelector("#captcha-mensaje")
-    if (captcha.innerHTML == valor.value) {
-        divoculto.classList.remove("validacion-captcha");
-        divoculto.classList.add("validacion-captcha-correcto");
-        divoculto.classList.remove("validacion-captcha-incorrecto");
+    if (captcha.innerHTML === inputUsuario.value) {
+        mensajeCaptcha.classList.remove("validacion-captcha");
+        mensajeCaptcha.classList.add("validacion-captcha-correcto");
+        mensajeCaptcha.classList.remove("validacion-captcha-incorrecto");
         textocaptcha.innerHTML= "Captcha Correcto";
     }
     else {
-        divoculto.classList.remove("validacion-captcha");
-        divoculto.classList.add("validacion-captcha-incorrecto");
-        divoculto.classList.remove("validacion-captcha-correcto");
+        mensajeCaptcha.classList.remove("validacion-captcha");
+        mensajeCaptcha.classList.add("validacion-captcha-incorrecto");
+        mensajeCaptcha.classList.remove("validacion-captcha-correcto");
         textocaptcha.innerHTML= "Captcha Incorrecto";
     }
 }
